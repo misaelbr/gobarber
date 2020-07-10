e87589001870131de95a5e338343a79cba3418ff
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2020-07-09 23:16:10 -0300

    Add injeção de dependência nos services

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointments';
@@ -16,8 +17,12 @@ interface IRequest {
   date: Date;
 }
 
+@injectable()
 class CreateAppointmentService {
-  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+  ) {}
 
   public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
--- src/modules/users/infra/http/routes/user.routes.ts
+++ src/modules/users/infra/http/routes/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -12,11 +12,9 @@ const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
-    const usersRepository = new UsersRepository();
     const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService(usersRepository);
-
+    const createUser = container.resolve(CreateUserService);
     const user = await createUser.execute({
       name,
       email,
@@ -35,8 +33,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const usersRepository = new UsersRepository();
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user_id = request.user.id;
 
