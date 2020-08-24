cf6d5ea688a6223497f56a5c32c3059efe26f4c4
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2020-08-24 18:26:33 -0300

    Fix serialization in the cache

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -25,13 +25,17 @@ class ListProviderService {
     let users = await this.cacheProvider.recover<User[]>(
       `providers-list:${user_id}`
     );
+
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
     }
 
-    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    await this.cacheProvider.save(
+      `providers-list:${user_id}`,
+      classToClass(users)
+    );
 
     return classToClass(users);
   }
